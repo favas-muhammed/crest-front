519cbeeb6cc23b0d84eaa191b3a5bb63cb032d0a
Favas Muhammed <favassakkeer13@gmail.com>
2025-08-20 18:31:18 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import "./index.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+console.log("Client ID:", process.env.REACT_APP_GOOGLE_CLIENT_ID);
+console.log("API URL:", process.env.REACT_APP_API_URL);
+
 root.render(
   <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
     <App />
